35b8515862dcd7b0860453c21f55f157eaf4a275
Will Anderson <will@Wills-iMac.local>
2017-03-16 08:58:54 -0700

    fixed mobile nav

--- js/app.js
+++ js/app.js
@@ -3,12 +3,13 @@ $(document).ready(function() {
   var header_height = $("#header").outerHeight(true);
   $("section").css("padding-top", header_height);
 
-  $("#work-btn").click(function(){
-      $(".wrapper").toggleClass("expand");
-      $('#work-btn').html($('#work-btn').text() == '×' ? 'Work' : '×')
+  $("#menuToggle").click(function(){
+      $("header").toggleClass("active");
+      // $(".index").toggleClass("hide");
+      $('#menuToggle').html($('#menuToggle').text() == 'Close' ? 'Menu' : 'Close')
   })
 
-  $('footer').footerReveal();
+  // $('footer').footerReveal();
 
 
 });
