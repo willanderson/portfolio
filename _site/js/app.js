f2bcbd9076d8a6ac60b60aff0239afab73dd0b97
Will Anderson <wmaxand@gmail.com>
2017-08-17 16:23:19 -0700

    tweaks on index layout

--- _site/js/app.js
+++ _site/js/app.js
@@ -12,7 +12,9 @@ $(document).ready(function() {
 
    $('#featured').slick({
      asNavFor: '#titles',
-     fade: true
+     fade: false,
+     prevArrow: "<button type='button' class='slick-prev'><svg class='slideshow-arrow-icon product-slider__arrow' viewBox='0 0 19 34' version='1.1' style='width: 19px; height: 24px;'><g stroke-width='1' stroke-linecap='round' stroke-linejoin='round' fill='none'><g transform='translate(-1042.000000, -2832.000000)'><g transform='translate(100.000000, 2489.000000)'><polyline points='943 343 960 360 943 377'></polyline></g></g></g></svg></button>",
+     nextArrow: "<button type='button' class='slick-next'><svg class='slideshow-arrow-icon product-slider__arrow' viewBox='0 0 19 34' version='1.1' style='width: 19px; height: 24px;'><g stroke-width='1' stroke-linecap='round' stroke-linejoin='round' fill='none'><g transform='translate(-1042.000000, -2832.000000)'><g transform='translate(100.000000, 2489.000000)'><polyline points='943 343 960 360 943 377'></polyline></g></g></g></svg></button>"
    });
 
    $('#titles').slick({
